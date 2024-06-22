b649dd9457015df921261cd26d3bbb614bcb13bc
to nguyen duc manh <ducmanh14032000@gmail.com>
2024-06-22 11:34:55 +0700

    bổ sung unit test bl

--- Job.BL/JobBL.cs
+++ Job.BL/JobBL.cs
@@ -1,13 +1,56 @@
-﻿namespace Job.BL
+﻿using Job.DL;
+
+namespace Job.BL
 {
     /// <summary>
     /// class job bl
     /// </summary>
     public class JobBL
     {
+        #region Constructor
+
         /// <summary>
         /// Hàm khởi tạo
         /// </summary>
         public JobBL() { }
+
+        #endregion
+
+        #region DL Create
+
+
+        /// <summary>
+        /// Đối tượng DL khai báo 1 lần
+        /// </summary>
+        private JobDL _dlObject;
+
+        /// <summary>
+        /// đối tượng DL để query
+        /// </summary>
+        private JobDL DLObject { 
+            get 
+            {
+                if(_dlObject == null)
+                {
+                    _dlObject = new JobDL();
+                }
+                return _dlObject; 
+            } 
+        }
+
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// lấy ra giá trị kích thước hiện tại của db
+        /// </summary>
+        public int GetCurrentSizeDB()
+        {
+            return DLObject.GetCurrentSizeDB();
+        }
+
+        #endregion
     }
 }
--- Job.Test/BL/JobBLTest.cs
+++ Job.Test/BL/JobBLTest.cs
@@ -1,9 +1,50 @@
-﻿namespace Job.Test.BL
+﻿using Job.BL;
+
+namespace Job.Test.BL
 {
     /// <summary>
-    /// Class test job BL
+    /// Class test job DL
     /// </summary>
+    [TestClass]
     public class JobBLTest
     {
+
+        #region FakeObject
+
+        private JobBLFake? _objectTest;
+
+        private JobBLFake ObjectTest
+        {
+            get
+            {
+                if (_objectTest == null)
+                {
+                    _objectTest = new JobBLFake();
+                }
+                return _objectTest;
+            }
+        }
+
+        #endregion
+
+        #region TestMethods
+
+        /// <summary>
+        /// Kiểm tra xem có kết nối được với db thành công không bằng cách trả về size của db
+        /// </summary>
+        [TestMethod]
+        public void GetCurrentSizeDB_returnCurrentSize()
+        {
+            int dbSize = ObjectTest.GetCurrentSizeDB();
+            Assert.IsTrue(dbSize > 0);
+        }
+
+        #endregion
+    }
+
+
+    public class JobBLFake : JobBL
+    {
+        public JobBLFake() : base() { }
     }
 }
