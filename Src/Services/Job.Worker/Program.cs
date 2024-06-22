68a2443b2cef6b877e8c908ec6e396fda25130ab
to nguyen duc manh <ducmanh14032000@gmail.com>
2024-06-22 12:11:04 +0700

    xóa file tạm

--- Src/Services/Job.Worker/Program.cs
+++ Src/Services/Job.Worker/Program.cs
@@ -1,7 +1,4 @@
-﻿using Quartz.Impl;
-using Quartz;
-using Quartz.Logging;
-using Job.Worker.Job;
+﻿using Quartz.Logging;
 using Job.Worker.Log;
 
 namespace Job.Worker
@@ -11,39 +8,6 @@ namespace Job.Worker
         private static async Task Main(string[] args)
         {
             LogProvider.SetCurrentLogProvider(new ConsoleLogProvider());
-
-            // Grab the Scheduler instance from the Factory
-            StdSchedulerFactory factory = new StdSchedulerFactory();
-            IScheduler scheduler = await factory.GetScheduler();
-
-            // and start it off
-            await scheduler.Start();
-
-            // define the job and tie it to our HelloJob class
-            IJobDetail job = JobBuilder.Create<HelloJob>()
-                .WithIdentity("job1", "group1")
-                .Build();
-
-            // Trigger the job to run now, and then repeat every 10 seconds
-            ITrigger trigger = TriggerBuilder.Create()
-                .WithIdentity("trigger1", "group1")
-                .StartNow()
-                .WithSimpleSchedule(x => x
-                    .WithIntervalInSeconds(10)
-                    .RepeatForever())
-                .Build();
-
-            // Tell Quartz to schedule the job using our trigger
-            await scheduler.ScheduleJob(job, trigger);
-
-            // some sleep to show what's happening
-            await Task.Delay(TimeSpan.FromSeconds(60));
-
-            // and last shut down the scheduler when you are ready to close your program
-            await scheduler.Shutdown();
-
-            Console.WriteLine("Press any key to close the application");
-            Console.ReadKey();
         }
 
     }
